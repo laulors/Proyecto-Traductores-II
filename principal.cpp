4ba6a1cdc232d962bb2f703439658a3517d5fb77
Lorena García <laura_lors_145@hotmail.com>
2017-09-24 22:28:06 -0500

    act1_traductoresII lexico.h principal.cpp lexico.cpp

---
+++ principal.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "lexico.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    Lexico lexico;
+	lexico.entrada("35.88 \"hola \" 33 \"124.lalalala#&\" if 20 / 12 = 32");
+
+    cout << "Resultado del Analisis Lexico" << endl << endl;
+    cout << "Simbolo\t\tTipo" << endl;
+
+    while ( lexico.simbolo.compare("$") != 0 ){
+          lexico.sigSimbolo();
+
+          cout <<  lexico.simbolo << "\t\t" << lexico.tipoAcad(lexico.tipo) << endl;
+    }
+
+    cin.get();
+
+    return 0;
+}
